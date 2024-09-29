2c82ca75ff9aebbbdf9601f776cb12c9f7f5c314
Raghad Almarri <raghad55.11@icloud.com>
2024-09-29 10:27:24 +0300

    Initial Commit

---
+++ profile/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  profile
+//
+//  Created by Raghad Mohammed Almarri on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Alen")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.orange, lineWidth: 4))
+                
+            Text("Raghad")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.orange)
+//                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom)
+            Text("The world is full of nice people if you can't find one, be one.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ profile/profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileApp.swift
+//  profile
+//
+//  Created by Raghad Mohammed Almarri on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
